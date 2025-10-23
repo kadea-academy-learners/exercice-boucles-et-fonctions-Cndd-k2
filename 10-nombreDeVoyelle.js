3bc4b5cffe5de41b9462930abb3063cf43325544
cndd-k2 <akacandidek2@gmail.com>
2025-10-23 18:00:41 +0100

    feat: implement nombreDeVoyelles function to count vowels in a given phrase

--- 10-nombreDeVoyelle.js
+++ 10-nombreDeVoyelle.js
@@ -7,12 +7,30 @@
     function nombreDeVoyelles(phrase) -> number
 
   Placeholder : levée d'erreur pour indiquer que l'exercice doit être implémenté.
-*/
-function nombreDeVoyelles(phrase) {
-  // Exercice non implémenté : doit compter les voyelles
+*/ 
+// Exercice non implémenté : doit compter les voyelles
   // Placeholder neutre : retourne 0 pour indiquer non-implémentation
-  return 0;
+function nombreDeVoyelles(phrase) {
+ let nombreDeVoyelle = 0;
+ for (let i = 0; i < phrase.length; i++){
+  if (phrase[i] == "a"){
+    nombreDeVoyelle = nombreDeVoyelle + 1; 
+    } else if (phrase[i] == "e") {
+      nombreDeVoyelle = nombreDeVoyelle + 1;
+      } else if (phrase[i] == "i") {
+        nombreDeVoyelle = nombreDeVoyelle + 1;
+        } else if (phrase[i] == "o"){
+          nombreDeVoyelle = nombreDeVoyelle + 1;
+          } else if (phrase[i] == "u"){
+            nombreDeVoyelle = nombreDeVoyelle + 1;
+            } else if (phrase[i] == "y"){
+              nombreDeVoyelle = nombreDeVoyelle + 1;
+            }
+  }
+  return nombreDeVoyelle;
 }
+
+console.log(nombreDeVoyelles("bonjour"));
 
 // Ne pas modifier la ligne ci-dessous
 module.exports = { nombreDeVoyelles }
